830fb314a4a3054b45c2a38a6b70528572a8adbe
zakir hossain <zakir.rars@gmail.com>
2022-02-21 22:50:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let searchBtn = document.querySelector('#searchBtn');
+let searchUser = document.querySelector('#searchUser');
+let ui = new UI();
+
+searchBtn.addEventListener('click', (e) => {
+    let userText = searchUser.value;
+    if (userText != '') {
+        //Fetch API
+        fetch(`https://api.github.com/users/${userText}`).then(result => result.json()).then(data => {
+            if(data.message=="Not Found"){
+                //show alert
+                ui.showAlert("User not found!","alert alert-danger");
+            }else{
+                //show profile
+                ui.showProfile(data);
+            }
+        });
+    }else{
+        //clear profile
+        ui.clearProfile();
+    }
+});
